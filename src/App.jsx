a6895020b12083d0aeae4c4a15e16e7b360880b1
Abraham10VelGlez <usbrifa@gmail.com>
2024-12-10 09:39:24 -0600

    daisyui_x_reactjs

---
+++ src/App.jsx
@@ -0,0 +1,309 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Link</a></li>
+            <li>
+              <label className="swap swap-flip text-9xl">
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
+
+                <div className="swap-on">😈</div>
+                <div className="swap-off">😇</div>
+              </label>
+            </li>
+            <li>
+              <label className="btn btn-circle swap swap-rotate">
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
+
+                {/* hamburger icon */}
+                <svg
+                  className="swap-off fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512">
+                  <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+                </svg>
+
+                {/* close icon */}
+                <svg
+                  className="swap-on fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 512 512">
+                  <polygon
+                    points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                </svg>
+              </label>
+            </li>
+            <li>
+              <label className="swap swap-rotate">
+                {/* this hidden checkbox controls the state */}
+                <input type="checkbox" />
+
+                {/* sun icon */}
+                <svg
+                  className="swap-on h-10 w-10 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                </svg>
+
+                {/* moon icon */}
+                <svg
+                  className="swap-off h-10 w-10 fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24">
+                  <path
+                    d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                </svg>
+              </label>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="bg-base-100 rounded-t-none p-2">
+                  <li><a>Link 1</a></li>
+                  <li><a>Link 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+      <div className="flex w-full flex-col">
+        <div className="divider">Default</div>
+        <div className="divider divider-neutral">Neutral</div>
+        <div className="divider divider-primary">Primary</div>
+        <div className="divider divider-secondary">Secondary</div>
+        <div className="divider divider-accent">Accent</div>
+        <div className="divider divider-success">Success</div>
+        <div className="divider divider-warning">Warning</div>
+        <div className="divider divider-info">Info</div>
+        <div className="divider divider-error">Error</div>
+      </div>
+
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide4" className="btn btn-circle">❮</a>
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide1" className="btn btn-circle">❮</a>
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide2" className="btn btn-circle">❮</a>
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            className="w-full" />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" className="btn btn-circle">❮</a>
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="mockup-window bg-base-300 border">
+        <div className="bg-base-200 flex justify-center px-4 py-16">
+          <button className="btn">Hello daisyUI</button>
+
+          <div className="collapse bg-base-200">
+            <input type="radio" name="my-accordion-1" defaultChecked />
+            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+          <div className="collapse bg-base-200">
+            <input type="radio" name="my-accordion-1" />
+            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+          <div className="collapse bg-base-200">
+            <input type="radio" name="my-accordion-1" />
+            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className="mockup-browser bg-base-300 border">
+        <div className="mockup-browser-toolbar">
+          <div className="input">https://daisyui.com</div>
+        </div>
+        <div className="bg-base-200 flex justify-center px-4 py-16">
+          <select className="select select-secondary w-full max-w-xs">
+            <option disabled selected>Pick your favorite language</option>
+            <option>Java</option>
+            <option>Go</option>
+            <option>C</option>
+            <option>C#</option>
+            <option>C++</option>
+            <option>Rust</option>
+            <option>JavaScript</option>
+            <option>Python</option>
+          </select>
+          <select className="select select-success w-full max-w-xs">
+            <option disabled selected>Pick your favorite anime</option>
+            <option>One Piece</option>
+            <option>Naruto</option>
+            <option>Death Note</option>
+            <option>Attack on Titan</option>
+            <option>Bleach</option>
+            <option>Fullmetal Alchemist</option>
+            <option>Jojo's Bizarre Adventure</option>
+          </select>
+
+          <div className="flex w-52 flex-col gap-4">
+            <div className="skeleton h-32 w-full"></div>
+            <div className="skeleton h-4 w-28"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+          </div>
+
+
+          <div className="flex w-52 flex-col gap-4">
+            <div className="flex items-center gap-4">
+              <div className="skeleton h-16 w-16 shrink-0 rounded-full"></div>
+              <div className="flex flex-col gap-4">
+                <div className="skeleton h-4 w-20"></div>
+                <div className="skeleton h-4 w-28"></div>
+              </div>
+            </div>
+            <div className="skeleton h-32 w-full"></div>
+          </div>
+
+
+          <progress className="progress progress-error w-56" value={0} max="100"></progress>
+          <progress className="progress progress-error w-56" value="10" max="100"></progress>
+          <progress className="progress progress-error w-56" value="40" max="100"></progress>
+          <progress className="progress progress-error w-56" value="70" max="100"></progress>
+          <progress className="progress progress-error w-56" value="100" max="100"></progress>
+
+        </div>
+      </div>
+
+      <div className="mockup-browser bg-base-300 border">
+        <div className="mockup-browser-toolbar">
+          <div className="input">https://daisyui.com</div>
+        </div>
+        <div className="bg-base-200 flex justify-center px-4 py-16">
+
+          <span className="loading loading-infinity loading-xs"></span>
+          <span className="loading loading-infinity loading-sm"></span>
+          <span className="loading loading-infinity loading-md"></span>
+          <span className="loading loading-infinity loading-lg"></span>
+
+
+
+          <span className="loading loading-spinner text-primary"></span>
+          <span className="loading loading-spinner text-secondary"></span>
+          <span className="loading loading-spinner text-accent"></span>
+          <span className="loading loading-spinner text-neutral"></span>
+          <span className="loading loading-spinner text-info"></span>
+          <span className="loading loading-spinner text-success"></span>
+          <span className="loading loading-spinner text-warning"></span>
+          <span className="loading loading-spinner text-error"></span>
+
+
+
+          <div className="card glass w-96">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="car!" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Life hack</h2>
+              <p>How to park your car at your garage?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Learn now!</button>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="avatar">
+            <div className="mask mask-squircle w-24">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+          <div className="avatar">
+            <div className="mask mask-hexagon w-24">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+          <div className="avatar">
+            <div className="mask mask-triangle w-24">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+
+
+
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <button className="btn" onClick={() => document.getElementById('my_modal_5').showModal()}>open modal</button>
+          <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">Press ESC key or click the button below to close</p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
